7e23b674d51efb8c9885a8269e24b2eea8c80c76
Emily Dederick <edederic@mines.edu>
2018-04-28 00:05:04 -0600

    cleaned up css file and made it so sections switch out when buttons are pressed

---
+++ navigator.js
@@ -0,0 +1,79 @@
+function showIntro() {
+  // update button classes
+  document.getElementById("intro_button").className = "current_page";
+  document.getElementById("rp_button").className = "dummy_class";
+  document.getElementById("helped_button").className = "dummy_class";
+  document.getElementById("define_button").className = "dummy_class";
+  document.getElementById("growth_button").className = "dummy_class";
+
+  // update section classes
+  document.getElementById("intro").className = "shown";
+  document.getElementById("about_rp").className = "dummy_class";
+  document.getElementById("how_helped").className = "dummy_class";
+  document.getElementById("poverty_privilege").className = "dummy_class";
+  document.getElementById("growth").className = "dummy_class";
+}
+
+function showRP() {
+  // update button classes
+  document.getElementById("intro_button").className = "dummy_class";
+  document.getElementById("rp_button").className = "current_page";
+  document.getElementById("helped_button").className = "dummy_class";
+  document.getElementById("define_button").className = "dummy_class";
+  document.getElementById("growth_button").className = "dummy_class";
+
+  // update section classes
+  document.getElementById("intro").className = "dummy_class";
+  document.getElementById("about_rp").className = "shown";
+  document.getElementById("how_helped").className = "dummy_class";
+  document.getElementById("poverty_privilege").className = "dummy_class";
+  document.getElementById("growth").className = "dummy_class";
+}
+
+function showHelped() {
+  // update button classes
+  document.getElementById("intro_button").className = "dummy_class";
+  document.getElementById("rp_button").className = "dummy_class";
+  document.getElementById("helped_button").className = "current_page";
+  document.getElementById("define_button").className = "dummy_class";
+  document.getElementById("growth_button").className = "dummy_class";
+
+  // update section classes
+  document.getElementById("intro").className = "dummy_class";
+  document.getElementById("about_rp").className = "dummy_class";
+  document.getElementById("how_helped").className = "shown";
+  document.getElementById("poverty_privilege").className = "dummy_class";
+  document.getElementById("growth").className = "dummy_class";
+}
+
+function showDefine() {
+  // update button classes
+  document.getElementById("intro_button").className = "dummy_class";
+  document.getElementById("rp_button").className = "dummy_class";
+  document.getElementById("helped_button").className = "dummy_class";
+  document.getElementById("define_button").className = "current_page";
+  document.getElementById("growth_button").className = "dummy_class";
+
+  // update section classes
+  document.getElementById("intro").className = "dummy_class";
+  document.getElementById("about_rp").className = "dummy_class";
+  document.getElementById("how_helped").className = "dummy_class";
+  document.getElementById("poverty_privilege").className = "shown";
+  document.getElementById("growth").className = "dummy_class";
+}
+
+function showGrowth() {
+  // update button classes
+  document.getElementById("intro_button").className = "dummy_class";
+  document.getElementById("rp_button").className = "dummy_class";
+  document.getElementById("helped_button").className = "dummy_class";
+  document.getElementById("define_button").className = "dummy_class";
+  document.getElementById("growth_button").className = "current_page";
+
+  // update section classes
+  document.getElementById("intro").className = "dummy_class";
+  document.getElementById("about_rp").className = "dummy_class";
+  document.getElementById("how_helped").className = "dummy_class";
+  document.getElementById("poverty_privilege").className = "dummy_class";
+  document.getElementById("growth").className = "shown";
+}
